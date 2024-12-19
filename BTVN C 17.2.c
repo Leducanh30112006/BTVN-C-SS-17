051ee956a9c5be91fbb716f61606adc08f5e2894
Leducanh30112006 <le1duc2anh3@gmail.com>
2024-12-19 12:57:39 +0700

    Update BTVN C 17.2.c

--- BTVN C 17.2.c
+++ BTVN C 17.2.c
@@ -1,116 +1,92 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 
-void addString(char *string);
-void reverseString(char *string);
-int countLetter(char *string);
-void compareString(char *string, char *string2);
-void toUpperString(char *string);
-void connectString(char *string,char *string3);
-int main() {
-    int choice;
-    char string[100];  
-    char string2[100];      
-    char string3[100];
-    do {
-        printf("                Menu                          \n");
-        printf("1. Nhap vao chuoi\n");
-        printf("2. In ra chuoi dao nguoc\n");
-        printf("3. Dem so luong tu trong chuoi\n");
-        printf("4. Nhap vao 1 chuoi va so sanh\n");
-        printf("5. In hoa tat ca cac chu cai\n");
-        printf("6. Nhap vao 1 chuoi va them vao chuoi ban dau\n");
-        printf("7. Thoat\n");
-        printf("Nhap lua chon cua ban : ");
-        scanf("%d", &choice);
-        fflush(stdin);
-        switch (choice) {
-        case 1:
-            addString(string);
-            break;
-        case 2:
-            printf("Chuoi dao nguoc: ");
-            reverseString(string);
-            break;
-        case 3:
-            printf("So luong tu trong chuoi la: %d\n", countLetter(string));
-            break;
-        case 4:
-            compareString(string, string2);
-            break;
-        case 5:
-            toUpperString(string);
-            break;
-        case 6:
-            connectString(string,string3);
-            break;
-        case 7:
-            printf("Tam biet!!\n");
-            break;
-        default:
-            printf("Khong hop le!! Nhap lai!!\n");
-            break;
-        }
-    } while (choice != 7);
-}
-
-void addString(char *string) {
-    printf("Nhap chuoi: ");
-    fgets(string, 100, stdin);
-    int size = strlen(string);
-    string[size-1] = '\0';
-}
-
-void reverseString(char *string) {
-    char *end = string + strlen(string) - 1; 
-    while (end >= string) {
-        printf("%c", *end);
-        end--;
+void nhapChuoi(char *str) {
+    printf("Nhap vao chuoi: ");
+    fgets(str, 100, stdin);
+    size_t len = strlen(str);
+    if (len > 0 && str[len - 1] == '\n') {
+        str[len - 1] = '\0';
     }
-    printf("\n");
 }
 
-int countLetter(char *string) {
+void inChuoi(char *str) {
+    printf("Chuoi: %s\n", str);
+}
+
+void demChuCai(char *str) {
     int count = 0;
-    while (*string) { 
-        if (*string == ' ') {
-            continue;
-        } else {
+    while (*str) {
+        if (isalpha(*str)) {
             count++;
         }
-        string++;
+        str++;
     }
-    return count;
+    printf("So luong chu cai: %d\n", count);
 }
 
-void compareString(char *string, char *string2) {
-    printf("Nhap chuoi thu 2: ");
-    fgets(string2, 100, stdin);
-    int size = strlen(string2);
-    string[size-1] = '\0';
-    int compare = strcmp(string, string2);
-    if (compare > 0) {
-        printf("Chuoi ban dau lon hon chuoi thu hai.\n");
-    } else if (compare < 0) {
-        printf("Chuoi ban dau nho hon chuoi thu hai.\n");
-    } else {
-        printf("Hai chuoi bang nhau.\n");
+void demChuSo(char *str) {
+    int count = 0;
+    while (*str) {
+        if (isdigit(*str)) {
+            count++;
+        }
+        str++;
     }
+    printf("So luong chu so: %d\n", count);
 }
 
-void toUpperString(char *string) {
-    char *ptr = string;
-    while (*ptr) {
-        *ptr = toupper(*ptr);
-        ptr++;
+void demKyTuDacBiet(char *str) {
+    int count = 0;
+    while (*str) {
+        if (!isalpha(*str) && !isdigit(*str)) {
+            count++;
+        }
+        str++;
     }
-    printf("Chuoi viet hoa: %s\n", string);
+    printf("So luong ky tu dac biet: %d\n", count);
 }
 
-void connectString(char *string,char *string3){
-    printf("nhap chuoi de noi : ");
-    fgets(string3,100,stdin);
-    strcat(string,string3);
-    printf("%s\n",string);
+int main() {
+    char str[100];
+    int luaChon;
+
+    do {
+        printf("\nMENU\n");
+        printf("1. Nhap vao chuoi\n");
+        printf("2. In ra chuoi\n");
+        printf("3. Dem so luong chu cai trong chuoi va in ra\n");
+        printf("4. Dem so luong chu so trong chuoi va in ra\n");
+        printf("5. Dem so luong ky tu dac biet trong chuoi va in ra\n");
+        printf("6. Thoat\n");
+        printf("Nhap lua chon: ");
+        scanf("%d", &luaChon);
+        getchar();
+
+        switch (luaChon) {
+            case 1:
+                nhapChuoi(str);
+                break;
+            case 2:
+                inChuoi(str);
+                break;
+            case 3:
+                demChuCai(str);
+                break;
+            case 4:
+                demChuSo(str);
+                break;
+            case 5:
+                demKyTuDacBiet(str);
+                break;
+            case 6:
+                printf("Thoat chuong trinh\n");
+                break;
+            default:
+                printf("Lua chon khong hop le, vui long chon lai!\n");
+                break;
+        }
+    } while (luaChon != 6);
+
+    return 0;
 }
